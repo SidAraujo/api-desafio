a1d8a7bba4f97fbed3846827aeb83c6201260cf3
Sidiney <sidiney01@gmail.com>
2021-05-22 21:38:11 -0300

    Adicação de rotas para solicitar e modificar pedidos

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import request
-from ..views import gestores, clientes, produtos
+from ..views import gestores, clientes, pp
 
 @app.route('/gestores', methods=['POST'])
 def post_gestor():
@@ -10,6 +10,22 @@ def post_gestor():
 def post_cliente():
     return clientes.post_cliente()
 
-@app.route('/produtos', methods=['POST'])
-def post_produtos():
-    return produtos.post_produto()
+@app.route('/products', methods = ['POST'])
+def post_product():
+    return pp.post_product()
+
+@app.route('/orders', methods=['POST'])
+def post_order():
+    return pp.post_order()
+
+@app.route('/orders/<email>', methods=['GET'])
+def get_orders(email):
+    return pp.get_orders(email)
+
+@app.route('/orders/<id>', methods=['PUT'])
+def put_order(id):
+   return pp.put_order(id) 
+
+@app.route('/test', methods = ['POST'])
+def test():
+    return pp.test()
