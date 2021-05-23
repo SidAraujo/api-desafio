c9fb7dd40da20c9b196c2e3a2f9652cc8093c033
Sidiney Araujo <sidiney01@gmail.com>
2021-05-23 20:24:24 -0300

    novas rotas para produtos e pedidos

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import request
-from ..views import gestores, clientes, pp
+from ..views import gestores, clientes, pp, helper
 
 @app.route('/gestores', methods=['POST'])
 def post_gestor():
@@ -10,21 +10,39 @@ def post_gestor():
 def post_cliente():
     return clientes.post_cliente()
 
-@app.route('/products', methods = ['POST'])
-def post_product():
-    return pp.post_product()
+@app.route('/produtos', methods = ['POST'])
+@helper.token_gestor_required
+def post_protudo(current_gestor):
+    return pp.post_produto()
 
-@app.route('/orders', methods=['POST'])
-def post_order():
-    return pp.post_order()
+@app.route('/produtos/<id>', methods=['PUT'])
+def put_produto(id):
+    return pp.put_produto(id)
 
-@app.route('/orders/<email>', methods=['GET'])
-def get_orders(email):
-    return pp.get_orders(email)
+@app.route('/produtos/<id>', methods=['DELETE'])
+def delete_produto(id):
+    return pp.delete_produto(id)
 
-@app.route('/orders/<id>', methods=['PUT'])
-def put_order(id):
-   return pp.put_order(id) 
+@app.route('/pedidos', methods=['POST'])
+@helper.token_required
+def post_pedido(current_cliente):
+    return pp.post_pedido()
+
+@app.route('/pedidos/<email>', methods=['GET'])
+def get_pedidos(email):
+    return pp.get_pedidos(email)
+
+@app.route('/pedidos/<id>', methods=['PUT'])
+def put_pedido(id):
+   return pp.put_pedido(id) 
+
+@app.route('/login', methods = ['POST'])
+def authenticate():
+    return helper.auth()
+
+@app.route('/gestor/login', methods = ['POST'])
+def authenticate_gestor():
+    return helper.auth_gestor()
 
 @app.route('/test', methods = ['POST'])
 def test():
