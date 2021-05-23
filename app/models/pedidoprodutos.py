df33cc32df7cdf76286a394b8d67f3440a77954b
Sidiney Araujo <sidiney01@gmail.com>
2021-05-23 20:18:34 -0300

    delete dos models anteriores

--- app/models/pedidoprodutos.py
+++
@@ -1,7 +0,0 @@
-from app import db, ma
-from . import produtos, pedidos
-
-pedidoprodutos = db.Table('PedidoProduto',
-    db.Column('pedido_id', db.Integer, db.ForeignKey('pedidos.id'), primary_key=True),
-    db.Column('produto_id', db.Integer, db.ForeignKey('produtos.id'), primary_key=True)
-)
